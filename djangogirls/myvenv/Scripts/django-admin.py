b949ab0a102279897dedc8f560be9fa69432d3a9
siyeon Kim <siyeonk27@gmail.com>
2021-01-12 14:22:21 +0900

    설치하기

---
+++ djangogirls/myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\owner\dev\pirogramming\piro14_djangogirls\djangogirls\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
